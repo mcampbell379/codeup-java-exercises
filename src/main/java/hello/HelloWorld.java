516275d8619afc25855e46ddede01039add830c1
mcampbell379 <campbell.mitchell45@gmail.com>
2023-02-07 10:14:06 -0600

    first commit

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // the program starts exec here
+        System.out.println("Hello World");
+    }
+}
