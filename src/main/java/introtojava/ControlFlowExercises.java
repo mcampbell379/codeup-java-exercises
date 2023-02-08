03fc065be12a2f2fe86e1c3dbd4179cb43e2d887
mcampbell379 <campbell.mitchell45@gmail.com>
2023-02-08 09:57:18 -0600

    added FizzBuzz exercise

--- src/main/java/introtojava/ControlFlowExercises.java
+++ src/main/java/introtojava/ControlFlowExercises.java
@@ -7,17 +7,27 @@ public class ControlFlowExercises {
 //            System.out.print(i + " ");
 //            i++;
 //        }
-        for (int i = 0; i <= 15; i++) {
-            System.out.print(i + " ");
-        }
+//        for (int i = 0; i <= 15; i++) {
+//            System.out.print(i + " ");
+//        }
 
 //        int itwos = 0;
 //        do {
 //            itwos += 2;
 //            System.out.printf("%n%3d", itwos);
 //        }while(itwos < 100);
-        for (int j = 0; j <= 100; j+=2) {
-            System.out.printf("%n%3d", j);
+//        for (int j = 0; j <= 100; j+=2) {
+//            System.out.printf("%n%3d", j);
+//        }
+
+        for (int i = 1; i <= 100; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if(i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else System.out.println(i);
         }
     }
 }
