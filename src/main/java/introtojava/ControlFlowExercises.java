c366237c2473398ad461939e4853697d196ea80e
mcampbell379 <campbell.mitchell45@gmail.com>
2023-02-08 09:50:02 -0600

    refactored to use for loops instead if while

--- src/main/java/introtojava/ControlFlowExercises.java
+++ src/main/java/introtojava/ControlFlowExercises.java
@@ -2,16 +2,22 @@ package introtojava;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while(i <= 15) {
+//        int i = 5;
+//        while(i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
+        for (int i = 0; i <= 15; i++) {
             System.out.print(i + " ");
-            i++;
         }
 
-        int itwos = 0;
-        do {
-            itwos += 2;
-            System.out.printf("%n%3d", itwos);
-        }while(itwos < 100);
+//        int itwos = 0;
+//        do {
+//            itwos += 2;
+//            System.out.printf("%n%3d", itwos);
+//        }while(itwos < 100);
+        for (int j = 0; j <= 100; j+=2) {
+            System.out.printf("%n%3d", j);
+        }
     }
 }
