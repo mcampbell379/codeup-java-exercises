d76e8de4edf657c2ff14fbe0a4a7dab70adfc2e3
mcampbell379 <campbell.mitchell45@gmail.com>
2023-02-09 11:43:17 -0600

    added multiline string example to the string exercise

--- src/main/java/StringExercise.java
+++ src/main/java/StringExercise.java
@@ -1,7 +1,10 @@
 public class StringExercise {
     public static void main(String[] args) {
         String noEducation = "We don't need no education" + "\n" + "We don't need no thought control";
-        String sInside = "Check \"this\" out!, \"s inside of \"s!";
+//        String sInside = "Check \"this\" out!, \"s inside of \"s!";
+        String sInside = """
+                Check "this" out!. "s indside of "s!
+                """;
         String windowsDrive = "In windows, the main drive is usually C:\\";
         String backslashes = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
 
@@ -13,6 +16,5 @@ public class StringExercise {
         System.out.println();
         System.out.println(backslashes);
         System.out.println();
-
     }
 }
