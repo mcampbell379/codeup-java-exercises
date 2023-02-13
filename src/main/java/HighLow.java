e902de8f8adfe0c4e4d18379579d57e658ccbe03
mcampbell379 <campbell.mitchell45@gmail.com>
2023-02-13 08:46:32 -0600

    updated highlow game and metthods exercises

--- src/main/java/HighLow.java
+++ src/main/java/HighLow.java
@@ -11,29 +11,21 @@ public class HighLow {
 
     public static void guessingGame() {
         int guessAnswer = randomNum(100);
-        int guessLimit = 5;
-        int guessesMade = 0;
         int userGuess;
         Scanner userInput = new Scanner(System.in);
-
-        for (int i = guessesMade; i < guessLimit;) {
+        while(true) {
             System.out.print("Enter a number between 1 and 100: ");
             userGuess = userInput.nextInt();
-            if(guessesMade >= guessLimit){
-                System.out.println("Sorry. You ran out of guesses.");
-            } else if (userGuess == guessAnswer) {
+            if (userGuess == guessAnswer) {
                 System.out.println("GOOD GUESS!");
                 System.out.println("ANSWER: " + guessAnswer);
                 break;
             } else if (guessAnswer > userGuess) {
                 System.out.println("HIGHER");
-                i++;
             } else {
                 System.out.println("LOWER");
-                i++;
             }
         }
-
 
         userInput.close();
     }
